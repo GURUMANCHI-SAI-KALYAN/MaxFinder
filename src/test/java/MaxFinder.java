0f838af091f7608715a82d5e3c90688ccf155da5
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-26 19:56:28 +0530

    REFACTORED : TO FIND THE MAX USING GENERIC METHOD

--- src/test/java/MaxFinder.java
+++ src/test/java/MaxFinder.java
@@ -5,20 +5,20 @@ public class MaxFinder {
     @Test
     public void givenThreeIntereg_ShouldReturnMaxvalue() {
         MaxFinder maxFinder = new MaxFinder();
-        int max = maxFinder.findMax(300, 400, 500);
-        Assert.assertEquals(500, max);
+        int max = maxFinder.findMaxValue(300, 400, 500);
+        Assert.assertEquals(400, max);
     }
 
     @Test
     public void givenThreeflot_ShouldReturnMaxvalue() {
         MaxFinder maxFinder = new MaxFinder();
-        Float max = maxFinder.findFloatMax(1.0f, 2.0f, 3.0f);
-        Assert.assertEquals(3.0, max,0);
+        Float max = maxFinder.findMaxValue(1.0f, 2.0f, 3.0f);
+        Assert.assertEquals(400.0, max, 0);
     }
     @Test
     public void givenThreeStringValue_ShouldReturnMaxvalue() {
         MaxFinder maxFinder = new MaxFinder();
-        String max =  maxFinder.findStringMax("grapes", "mango", "orange");
-        Assert.assertEquals("orange", max);
+        String max = maxFinder.findMaxValue("grapes", "mango", "orange");
+        Assert.assertEquals("Peach", max);
     }
 }
