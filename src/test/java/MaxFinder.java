44de382502d94f0cf6df85d7cec5e1c0586cba4d
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-26 19:15:40 +0530

    ADDED : TO FIND MAX OF THREE FLOAT VALUES KEEP MAX AT 1ST PLACE

--- src/test/java/MaxFinder.java
+++ src/test/java/MaxFinder.java
@@ -1,11 +1,18 @@
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MaxFinder {
     @Test
-    public void givenThreeInteger_ShouldReturnMaxvalue() {
-        MaxFinder maxFinder= new MaxFinder();
+    public void givenThreeIntereg_ShouldReturnMaxvalue() {
+        MaxFinder maxFinder = new MaxFinder();
         int max = maxFinder.findMax(300, 400, 500);
-        Assert.assertEquals(500,max);
+        Assert.assertEquals(500, max);
+    }
+
+    @Test
+    public void givenThreeflot_ShouldReturnMaxvalue() {
+        MaxFinder maxFinder = new MaxFinder();
+        Float max = maxFinder.findFloatMax(3.0f, 2.0f, 1.0f);
+        Assert.assertEquals(3.0, max,0);
     }
 }
