446f0c5a0f28c5f00abc44897c80a2d46ab33cf7
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-26 20:15:59 +0530

    REFACTORED : TO FIND THE MAX USING GENERIC CLASS METHOD

--- src/main/java/MaxFinder.java
+++ src/main/java/MaxFinder.java
@@ -1,12 +1,22 @@
-public class MaxFinder {
-    public <E extends Comparable> E findMaxValue(E a, E b, E c) {
-        E max = a;
-        if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
-            max = b;
-        else if (c.compareTo(max) > 0)
-            max = c;
-        return max;
+ public class MaxFinder<E extends Comparable<E>> {
+        private E a, b, c;
+
+        public MaxFinder(E a, E b, E c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public E maximum() {
+            return findMaxValue(a, b, c);
+        }
+
+        public static <E extends Comparable> E findMaxValue(E a, E b, E c) {
+            E max = a;
+            if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
+                max = b;
+            else if (c.compareTo(max) > 0)
+        }
     }
-}
 
 
--- src/test/java/MaxFinder.java
+++ src/test/java/MaxFinder.java
@@ -2,23 +2,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaxFinder {
+
+
     @Test
     public void givenThreeIntereg_ShouldReturnMaxvalue() {
-        MaxFinder maxFinder = new MaxFinder();
-        int max = maxFinder.findMaxValue(300, 400, 500);
-        Assert.assertEquals(400, max);
+        MaxFinder maxFinder = new MaxFinder(300, 400, 500);
+        int max = (int) maxFinder.maximum();
+        Assert.assertEquals(500, max);
     }
 
     @Test
     public void givenThreeflot_ShouldReturnMaxvalue() {
-        MaxFinder maxFinder = new MaxFinder();
-        Float max = maxFinder.findMaxValue(1.0f, 2.0f, 3.0f);
-        Assert.assertEquals(400.0, max, 0);
+        MaxFinder maxFinder = new MaxFinder(1.0f, 2.0f, 3.0f);
+        Float max= (Float) maxFinder.maximum();
+        Assert.assertEquals(3.0, max, 0);
     }
     @Test
     public void givenThreeStringValue_ShouldReturnMaxvalue() {
-        MaxFinder maxFinder = new MaxFinder();
-        String max = maxFinder.findMaxValue("grapes", "mango", "orange");
-        Assert.assertEquals("Peach", max);
+        MaxFinder maxFinder = new MaxFinder("grapes", "mango", "orange");
+        String max = (String) maxFinder.maximum();
+        Assert.assertEquals("orange", max);
     }
 }
