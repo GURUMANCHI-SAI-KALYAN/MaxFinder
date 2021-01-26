235059ede75aa619ce0b9e825e7fae42821aafc8
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-26 20:26:42 +0530

    ADDED : Extended more than three parameters program

--- src/main/java/MaxFinder.java
+++ src/main/java/MaxFinder.java
@@ -1,22 +1,41 @@
- public class MaxFinder<E extends Comparable<E>> {
-        private E a, b, c;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-        public MaxFinder(E a, E b, E c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
+public class MaxFinder<E extends Comparable<E>> {
+    private E a, b, c;
 
-        public E maximum() {
-            return findMaxValue(a, b, c);
-        }
-
-        public static <E extends Comparable> E findMaxValue(E a, E b, E c) {
-            E max = a;
-            if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
-                max = b;
-            else if (c.compareTo(max) > 0)
-        }
+    public MaxFinder() {
     }
 
+    public MaxFinder(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
+    public E maximum() {
+        return findMaxValue(a, b, c);
+    }
+
+    public static <E extends Comparable> E findMaxValue(E a, E b, E c) {
+        E max = a;
+        if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
+            max = b;
+        else if (c.compareTo(max) > 0)
+            max = c;
+        MaxFinder.printmax(max);
+        return max;
+    }
+
+    public static <E> E max(E... args) {
+        List<E> collect = Arrays.stream(args).sorted().collect(Collectors.toList());
+        return collect.get(collect.size()-1);
+
+    }
+
+    private static <E> void printmax(E max) {
+        System.out.println(max + " is maximum");
+    }
+
+}
--- src/test/java/MaxFinder.java
+++ src/test/java/MaxFinder.java
@@ -1,26 +1,48 @@
+
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MaxFinder {
 
-
+class MaxFinderTest {
     @Test
-    public void givenThreeIntereg_ShouldReturnMaxvalue() {
+    public void givenThreeInteger_ShouldReturnMaxvalue() {
         MaxFinder maxFinder = new MaxFinder(300, 400, 500);
         int max = (int) maxFinder.maximum();
         Assert.assertEquals(500, max);
     }
 
     @Test
-    public void givenThreeflot_ShouldReturnMaxvalue() {
+    public void givenThreeFloatValue_ShouldReturnMaxvalue() {
         MaxFinder maxFinder = new MaxFinder(1.0f, 2.0f, 3.0f);
         Float max= (Float) maxFinder.maximum();
         Assert.assertEquals(3.0, max, 0);
     }
+
     @Test
     public void givenThreeStringValue_ShouldReturnMaxvalue() {
         MaxFinder maxFinder = new MaxFinder("grapes", "mango", "orange");
         String max = (String) maxFinder.maximum();
+        Assert.assertEquals("orange", max);
+    }
+
+    @Test
+    public void givenFiveIntegerinVarargs_ShouldReturnMaxvalue() {
+        MaxFinder maxFinder = new MaxFinder();
+        int max = (int) maxFinder.max(30, 10, 50,500,400);
+        Assert.assertEquals(500, max);
+    }
+
+    @Test
+    public void givenFiveFloatinVarargs_ShouldReturnMaxvalue() {
+        MaxFinder maxFinder = new MaxFinder();
+        float max = (float) maxFinder.max(58.5f, 787.5f, 445.25f,885.2f,487.36f);
+        Assert.assertEquals(885.2f, max,0);
+    }
+
+    @Test
+    public void givenSixStringinVarargs_ShouldReturnMaxvalue() {
+        MaxFinder maxFinder = new MaxFinder();
+        String  max = (String) maxFinder.max("grapes","mango","Orange","kwie","jack","lemon");
         Assert.assertEquals("orange", max);
     }
 }
