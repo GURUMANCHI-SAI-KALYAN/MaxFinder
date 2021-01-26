1c8c052f538e399cc0c636258a2bd035efecf2be
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-26 19:01:40 +0530

    REFACTORED : TO FIND MAX OF THREE INTEGERS KEEP MAX AT 2ND PLACE

--- src/test/java/MaxFinder.java
+++ src/test/java/MaxFinder.java
@@ -5,7 +5,7 @@ public class MaxFinder {
     @Test
     public void givenThreeInteger_ShouldReturnMaxvalue() {
         MaxFinder maxFinder= new MaxFinder();
-        int max = maxFinder.findMax(300, 200, 400);
+        int max = maxFinder.findMax(300, 400, 500);
         Assert.assertEquals(400,max);
     }
 }
